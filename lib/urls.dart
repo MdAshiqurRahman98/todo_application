293395690e680de903884930f500e28ff913bcb1
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2026-03-01 16:05:31 +0600

    urls file updated

---
+++ lib/urls.dart
@@ -0,0 +1,9 @@
+class Urls {
+  static const String _baseUrl = 'https://todo-restapi-crud-render.onrender.com';
+
+  static const getPost = '$_baseUrl/get_all_todo';
+  static String getTodoById({required String id}) => '$_baseUrl/get_todo_by_id/$id';
+  static const createTodo = '$_baseUrl/create_todo';
+  static String updateTodo({required String id}) => '$_baseUrl/update_todo_by_id/$id';
+  static String deleteTodo(String id) => '$_baseUrl/delete_todo_by_id/$id';
+}
